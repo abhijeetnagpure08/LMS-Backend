4eaed162df7ed1f1f94d51c32f45f346e7e5b40d
abhijeetnagpure08 <abhijeetnagpureofficial@gmail.com>
2024-12-11 12:27:16 +0530

    User signup, login, schema and model create

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config({});
+
+connectDB()
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use("/api/v1/user",userRoute);
+
+app.get("/home",(req,res)=> {
+    res.status(200).json({
+        success:true,
+        message: "Hello i am coming from backend"
+    })
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running at port ${PORT}`);
+    
+})
