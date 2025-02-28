870b241dad5c11a4add5d7b23723edb5fc41f427
abhijeetnagpure08 <abhijeetnagpureofficial@gmail.com>
2025-02-28 18:31:09 +0530

    Cors updated

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(cors())
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }))
 
 app.use("/api/v1/media",mediaRoute);
 app.use("/api/v1/user",userRoute);
